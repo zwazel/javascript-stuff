da8ecd65ef8a377420f4a27570d1b75238580042
Tim Irmler <zwazel@hotmail.com>
2021-09-03 15:32:21 +0200

    firework fountain or something

--- pages/particles/emitter.js
+++ pages/particles/emitter.js
@@ -5,6 +5,8 @@ window.onload = function () {
 function simulate() {
     let amountParticle = +document.getElementById("amountParticle").value;
     let randomColors = document.getElementById("randomColor").checked;
+    let emitAgain = document.getElementById("reemit").checked;
+    let gravity = +document.getElementById("gravity").value;
 
     let canvas = document.getElementById("canvas"),
         context = canvas.getContext("2d"),
@@ -13,7 +15,7 @@ function simulate() {
         p = [];
 
     for (let i = 0; i < amountParticle; i++) {
-        let newParticle = particle.create(width / 2, height / 2, 3, Math.random() * Math.PI * 2);
+        let newParticle = particle.create(width / 2, height, Math.random() * 8 + 5, -Math.PI / 2 + (Math.random()  * .2 - .1), gravity);
         newParticle.radius = Math.random() * 10 + 2;
         if(randomColors) {
             newParticle.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -34,18 +36,16 @@ function simulate() {
             context.arc(currentParticle.position.getX(), currentParticle.position.getY(), currentParticle.radius, 0, Math.PI * 2)
             context.fill();
 
-            if (currentParticle.position.getX() - currentParticle.radius > width) {
-                currentParticle.position.setX(-currentParticle.radius);
-            }
-            if (currentParticle.position.getX() + currentParticle.radius < 0) {
-                currentParticle.position.setX(width + currentParticle.radius);
-            }
-
             if (currentParticle.position.getY() - currentParticle.radius > height) {
-                currentParticle.position.setY(-currentParticle.radius);
-            }
-            if (currentParticle.position.getY() + currentParticle.radius < 0) {
-                currentParticle.position.setY(height + currentParticle.radius);
+                if(emitAgain) {
+                    currentParticle.position.setX(width / 2)
+                    currentParticle.position.setY(height);
+                    currentParticle.velocity.setLength(Math.random() * 8 + 5);
+                    currentParticle.velocity.setAngle(-Math.PI / 2 + (Math.random() * .2 - .1));
+                } else {
+                    let indexOfParticle = p.indexOf(currentParticle);
+                    p.splice(indexOfParticle, 1);
+                }
             }
         }
 
