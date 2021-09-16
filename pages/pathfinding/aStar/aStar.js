39cad94ea9ed315ef1b6257bc1a29a8cfa5f0eb4
Tim Irmler <zwazel@hotmail.com>
2021-09-16 11:42:03 +0200

    procedural grid

--- pages/pathfinding/aStar/aStar.js
+++ pages/pathfinding/aStar/aStar.js
@@ -2,17 +2,28 @@ let gridContainer = document.getElementById("gridContainer");
 let rowInput = document.getElementById("inputRows");
 let colInput = document.getElementById("inputCols");
 
-for (let i = 0; i < rowInput.value; i++) {
-    for (let j = 0; j < colInput.value; j++) {
-
+createGrid();
+function createGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.lastChild.remove();
     }
+
+    for (let i = 0; i < rowInput.value; i++) {
+        let unorderedList = document.createElement('ul');
+        unorderedList.classList.add(`col${i}`);
+        for (let j = 0; j < colInput.value; j++) {
+            let listItem = document.createElement('li');
+            listItem.classList.add('dot');
+            unorderedList.append(listItem);
+        }
+        gridContainer.append(unorderedList);
+    }
+
+    $('.dot').mouseenter(function () {
+        $(this).css({borderRadius: 0});
+        $(this).css({background: '#20B2AA'});
+    }).click(function () {
+        $(this).css({borderRadius: '50%'});
+        $(this).css({background: '#66CDAA'});
+    });
 }
-
-
-$('.dot').mouseenter(function () {
-    $(this).css({borderRadius: 0});
-    $(this).css({background: '#20B2AA'});
-}).click(function () {
-    $(this).css({borderRadius: '50%'});
-    $(this).css({background: '#66CDAA'});
-});
