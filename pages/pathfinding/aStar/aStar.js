dcb0dc859351228cdc0a97ec3d5c15ac21719c88
Tim Irmler <zwazel@hotmail.com>
2021-09-16 12:04:58 +0200

    working on types

--- pages/pathfinding/aStar/aStar.js
+++ pages/pathfinding/aStar/aStar.js
@@ -1,8 +1,67 @@
 let gridContainer = document.getElementById("gridContainer");
 let rowInput = document.getElementById("inputRows");
 let colInput = document.getElementById("inputCols");
+let blockTypeContainer = document.getElementById("blockTypeContainer");
 
-createGrid();
+window.onload = function () {
+    initBlockTypes();
+    createGrid();
+};
+
+class blockType {
+    _myColor;
+    _square;
+    _description;
+
+    constructor(myColor = '000000', square = true, description = 'undefined') {
+        this._myColor = myColor;
+        this._square = square;
+        this._description = description;
+    }
+
+    get myColor() {
+        return this._myColor;
+    }
+
+    set myColor(value) {
+        this._myColor = value;
+    }
+
+    get square() {
+        return this._square;
+    }
+
+    set square(value) {
+        this._square = value;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    set description(value) {
+        this._description = value;
+    }
+
+    toHTML() {
+        let div = document.createElement('div');
+        div.classList.add('blockType', this.description);
+        let spanDesc = document.createElement('span');
+        spanDesc.classList.add('description');
+        spanDesc.innerText = this.description;
+        div.append(spanDesc);
+        div.setAttribute('style', `background-color: #${this.myColor}`);
+        return div;
+    }
+}
+
+function initBlockTypes() {
+    let wall = new blockType();
+    let start = new blockType('71eb34', false, 'start');
+
+    blockTypeContainer.append(wall.toHTML(), start.toHTML());
+}
+
 function createGrid() {
     while (gridContainer.firstChild) {
         gridContainer.lastChild.remove();
